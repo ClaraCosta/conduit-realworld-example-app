d06d44f79504d66a496cfc2654c87e6b981a636d
clara <89088484+ClaraCosta@users.noreply.github.com>
2024-08-24 14:53:26 -0300

    Completando configurações de registro de novo usuario

--- testAutomation/cypress/e2e/pages/register/elements.js
+++ testAutomation/cypress/e2e/pages/register/elements.js
@@ -1,4 +1,6 @@
 export const ELEMENTS = {
     usernameInput: 'input[name="username"]',
+    emailInput: 'input[name="email"]',
+    passwordInput: 'input[name="password"]',
     signUpAccessLinkText: 'Sign up',
 };
